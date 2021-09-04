c8a6f7476a24e56a6aad0db873ca1837f65ebae1
Matheus Jacobina <71132039+matheusjacobina-tech@users.noreply.github.com>
2021-09-04 18:38:52 -0300

    commit inicial

---
+++ src/main/java/com/github/ApiSalasDeReuniao/ApiSalasDeReuniaoApplication.java
@@ -0,0 +1,13 @@
+package com.github.ApiSalasDeReuniao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSalasDeReuniaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSalasDeReuniaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/ApiSalasDeReuniao/ApiSalasDeReuniaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ApiSalasDeReuniao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSalasDeReuniaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
